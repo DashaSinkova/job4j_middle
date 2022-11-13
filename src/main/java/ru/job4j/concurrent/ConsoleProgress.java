fd35321be15002616813ed5b4822b315ceaae3c4
DashaPC <miss.sinkova-dasha@yandex.ru>
2022-11-13 14:11:16 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        String[] symbols = new String[]{"\\", "|", "/"};
+        int count = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (count == symbols.length) {
+                    count = 0;
+                }
+                System.out.print("\r load: " + symbols[count++]);
+                Thread.sleep(500);
+            }
+        }
+        catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+    }
+
